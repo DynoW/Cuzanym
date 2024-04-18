12b579cc414310f1959e12220819b48078a3e5ca
DynoW <104089032+DynoW@users.noreply.github.com>
2024-04-18 17:53:29 +0300

    modified:   pages/forum/index.vue

--- pages/forum/index.vue
+++ pages/forum/index.vue
@@ -2,7 +2,7 @@
 const user = useSupabaseUser()
 const client = useSupabaseClient()
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "~/node_modules/@prisma/client";
 
 const prisma = new PrismaClient();
 
