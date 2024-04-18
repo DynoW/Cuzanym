618d17a4d08434ae6a89a7bfeb79c20e937b98f5
DynoW <104089032+DynoW@users.noreply.github.com>
2024-04-18 16:43:09 +0300

    modified:   server/api/get/posts/index.ts

--- server/api/get/posts/index.ts
+++ server/api/get/posts/index.ts
@@ -1,8 +1,6 @@
 // ???
 import { serverSupabaseUser } from "#supabase/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from '~/prisma/db'
 
 export default defineEventHandler(async (event) => {
     const user = await serverSupabaseUser(event);
