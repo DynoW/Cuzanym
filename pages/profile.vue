ad9543451047a64fa9d337463dac3a17259fbeb5
DynoW <104089032+DynoW@users.noreply.github.com>
2024-04-21 11:26:55 +0300

    modified:   pages/profile.vue

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
-const client = useSupabaseClient()
+const { auth } = useSupabaseClient()
 
 const { data } = await useFetch("/api/get/user", {
     headers: useRequestHeaders(['cookie'])
 });
 const user = data.value;
+
+const handleLogout = async () => {
+    await auth.signOut()
+    navigateTo('/')
+}
 </script>
 
 <template>
     <NuxtLayout name="home">
         <main class="main">
-            <div class="basis-2/5 xl:basis-1/4 flex flex-col gap-4 bg-white rounded-lg p-5">
+            <div class="basis-2/5 xl:basis-1/4 flex flex-col gap-4 rounded-lg p-5 bg-white dark:bg-slate-800">
                 <div class="flex flex-col justify-center text-center">
                     <h1 class="font-extrabold text-2xl">
                         Profile
@@ -23,8 +28,8 @@ const user = data.value;
                     <h2>
                         Username:
                     </h2>
-                    <span title="username" class="input inline-block whitespace-nowrap overflow-hidden max-w-48" role="textbox"
-                        contenteditable>
+                    <span title="username" class="input inline-block whitespace-nowrap overflow-hidden max-w-48"
+                        role="textbox" contenteditable>
                         {{ (user?.username == null) ? 'not set' : user?.username }}
                     </span>
                     <button>
@@ -32,7 +37,7 @@ const user = data.value;
                     </button>
                 </div>
                 <div class="text-center md:text-start">
-                    <button @click="client.auth.signOut" class="text-red-500 font-extrabold text-lg">Sign out</button>
+                    <button @click="handleLogout()" class="text-red-500 font-extrabold text-lg">Sign out</button>
                 </div>
             </div>
             <div class="basis-3/5 xl:basis-3/4">
