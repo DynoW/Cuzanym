0128ad54db781d52ecc570840a53a2422288311e
DynoW <104089032+DynoW@users.noreply.github.com>
2024-04-22 13:47:44 +0300

    modified:   app.vue

--- app.vue
+++ app.vue
@@ -7,8 +7,8 @@ let qr = ref(false)
   <NuxtLayout name="default">
     <NuxtPage />
   </NuxtLayout>
-  <div class="hidden md:flex flex-col fixed bottom-0 right-0 max-h-64 max-w-64">
+  <div class="hidden md:flex flex-col fixed bottom-0 right-0">
     <button @click="qr = !qr" class="text-end">X</button>
-    <NuxtImg v-if="qr" src="/img/qr.png" alt="QR" />
+    <NuxtImg v-if="qr" src="/img/qr.png" alt="QR" class="max-h-64 max-w-64" />
   </div>
 </template>
