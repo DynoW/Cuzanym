26a0894ae434929c72c5b88153140a04b883f190
DynoW <104089032+DynoW@users.noreply.github.com>
2024-04-22 13:39:34 +0300

    modified:   app.vue 	new file:   public/img/qr.png

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
+<script setup lang="ts">
+let qr = ref(false)
+</script>
+
 <template>
   <NuxtLoadingIndicator color="blue" />
   <NuxtLayout name="default">
     <NuxtPage />
   </NuxtLayout>
+  <div class="hidden md:flex flex-col fixed bottom-0 right-0 max-h-64 max-w-64">
+    <button @click="qr = !qr" class="text-end">X</button>
+    <NuxtImg v-if="qr" src="/img/qr.png" alt="QR" />
+
+  </div>
 </template>
-
-<script setup>
-
-</script>
