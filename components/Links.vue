9edf03761b6b292a79d685102babcfbcbb6e82b7
DynoW <104089032+DynoW@users.noreply.github.com>
2024-05-26 15:31:39 +0300

    modified:   components/Links.vue 	modified:   pages/evenimente.vue

--- components/Links.vue
+++ components/Links.vue
@@ -1,6 +1,6 @@
 <template>
     <NuxtLink to="/forum" class="text-lg text-shadow">Forum</NuxtLink>
-    <NuxtLink to="/work-in-progress" class="text-lg text-shadow">Evenimente</NuxtLink>
+    <NuxtLink to="/evenimente" class="text-lg text-shadow">Evenimente</NuxtLink>
     <NuxtLink to="/cluburi" class="text-lg text-shadow">Cluburi</NuxtLink>
     <NuxtLink to="/forum/raportări" class="text-lg text-shadow">Raportări</NuxtLink>
     <NuxtLink to="/terms" class="text-lg text-shadow">Termenii</NuxtLink>
--- pages/evenimente.vue
+++ pages/evenimente.vue
@@ -1,13 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
     <NuxtLayout name="home">
         <nav>
             <Hierarchy />
         </nav>
         <main class="main">
-            <div class="">
+            <div class="grid grid-cols-1 gap-5 md:grid-cols-3 md:gap-24">
                 <ContentList path="/events" v-slot="{ list }">
-                    <div v-for="event in list" :key="event._path">
-                        <NuxtImg :src="event.imagine" :alt="event.titlu" :id="event.titlu" class="max-w-80" />
+                    <div v-for="event in list" :key="event._path" class="duration-400 ease-in-out hover:scale-105">
+                        <NuxtLink to="/work-in-progress">
+                            <NuxtImg :src="event.imagine" :alt="event.titlu" :id="event.titlu"
+                                class="aspect-[1/1.4]  object-contain hover:object-contain" />
+                        </NuxtLink>
                     </div>
                 </ContentList>
             </div>
